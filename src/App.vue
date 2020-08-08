36ff405a9ef82a4272d37f7a3fc9273a819584c2
harshild <harshil.dhariwal@gmail.com>
2020-08-07 20:08:15 -0400

    Added logo and contact icons Some changes to the main layout .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Header/>
   </div>
 </template>
@@ -9,7 +8,7 @@
   import Header from "@/components/Header";
 
   export default {
-    name: 'App',
+    name: 'PageHeader',
     components: {
       Header
     }
@@ -23,6 +22,5 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        Header
+    <div class="container-header">
+        <div class="content">
+            <div class="logo">
+                <a href="http://remoteroofing.com">
+                    <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f12985a91b23596370f7a81_logo-72-dc-7-f-0-f%402x.png" height="50" >
+                </a>
+            </div>
+
+            <div class="contact">
+                <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f1297d1c3fecf36271c10a2_483947%403x.png">
+                <a href="tel:2148658818">
+                    214-865-8818
+                </a>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -10,5 +24,24 @@
     }
 </script>
 <style scoped>
+    /*Need to check min height*/
+    .container-header {
+        background-image: url("https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f127dcece36df2cd5e4f713_Screen%20Shot%202020-07-17%20at%209.41.58%20PM.png");
+        min-height: 500px;
+        padding-top: 20px;
+    }
 
+    .container-header .content {
+        margin: auto;
+        max-width: 964px;
+    }
+
+    .container-header .content .logo {
+        display: inline-block;
+        float: left;
+    }
+    .container-header .content .contact {
+        float: right;
+        display: flex;
+    }
 </style>
